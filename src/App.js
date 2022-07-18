7a0ec089d6868a2fe5569822a1558bf5b8d0d7f5
Manohar Kumar Thakur <manoharmkt.182@gmail.com>
2022-07-18 15:50:43 +0530

    Application created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Weather from "./components/Weather";
+import "./App.css";
 
 function App() {
   return (
     <>
-    <div className="container">
-
-      
-    </div>
+      <Weather />
     </>
   );
 }
---
+++ src/components/Weather.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+import "../style.css";
+
+const Weather = () => {
+  const [city, setCity] = useState(null);
+  const [search, setSearch] = useState("Mumbai");
+
+  useEffect(() => {
+    const fetchApi = async () => {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=d9272fbec9f23a662b5d363fe5bf166a`;
+      const response = await fetch(url);
+      const resJson = await response.json();
+      // console.log(resJson);
+      setCity(resJson.main);
+    };
+
+    fetchApi();
+  }, [search]);
+
+  return (
+    <>
+      <div className="box">
+        <div className="inputData">
+          <input
+            type="search"
+            value={search}
+            className="inputField"
+            onChange={(event) => {
+              setSearch(event.target.value);
+            }}
+          />
+        </div>
+        {!city ? (
+          <p className="errorMsg">No data found</p>
+        ) : (
+          <div>
+            <div className="info">
+              <h2 className="location">
+                <i className="fas fa-street-view"></i>
+                {search}
+              </h2>
+              <h1 className="temp"> {city.temp}°cel</h1>
+              <h3 className="tempmin_max">
+                Min: {city.temp_min}°cel | Max: {city.temp_max}°cel
+              </h3>
+            </div>
+            <div className="wave -one"></div>
+            <div className="wave -two"></div>
+            <div className="wave -three"></div>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Weather;
